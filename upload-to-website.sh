fa8604b03b968bae74041417fa93fe56dce584b6
William Wang <wow7@cornell.edu>
2020-09-01 14:20:45 -0400

    update upload script for server migration

--- upload-to-website.sh
+++ upload-to-website.sh
@@ -4,10 +4,14 @@ GREEN="\033[0;32m"
 OFF="\033[0m"
 
 SERVER="acsu.cornell.edu"
-SERVER_PATH="/var/www/html"
+SERVER_PATH="/var/www/acsu"
+
+echo Enter your NetID, you will be asked for your Cornell password:
+
+read NETID
 
 jekyll build
 
 printf "Deploying to ${GREEN}${SERVER}${SERVER_PATH}${OFF}\n"
 
-rsync -rvz _site/ webmaster@$SERVER:$SERVER_PATH
+rsync -rvz _site/ $NETID@$SERVER:$SERVER_PATH
